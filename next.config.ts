39087841c34cd6e95b67b371b8717233fdcc1f8b
mon7792 <monish7792@gmail.com>
2025-06-28 17:33:36 +0100

    feat: security: + csp protection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://avatars.githubusercontent.com;
+    font-src 'self' https://esm.sh;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
 const nextConfig: NextConfig = {
   transpilePackages: ['@excalidraw/excalidraw', 'use-debounce'],
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,8 +9,20 @@ const csrfMiddleware = createCsrfMiddleware({
 
 export const middleware = csrfMiddleware;
 
-// export const config = {
-//     matcher: [
-//         '/api/diagram/:path*', // Apply middleware to /api/diagram/*
-//     ],
-// };
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: '/((?!_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
+}
